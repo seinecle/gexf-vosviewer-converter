921050d1358f7c1ff0545704c262002600404658
seinecle <1244100+seinecle@users.noreply.github.com>
2024-03-18 14:44:04 +0100

    fixed the vv to gexf converter, which actually never worked

--- src/main/java/net/clementlevallois/gexfvosviewerjson/VOSViewerJsonToGexf.java
+++ src/main/java/net/clementlevallois/gexfvosviewerjson/VOSViewerJsonToGexf.java
@@ -17,6 +17,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringReader;
+import java.io.StringWriter;
 import java.nio.file.Path;
 import java.util.Calendar;
 import java.util.HashMap;
@@ -33,6 +34,8 @@ import org.gephi.graph.api.Graph;
 import org.gephi.graph.api.GraphController;
 import org.gephi.graph.api.GraphModel;
 import org.gephi.graph.api.Node;
+import org.gephi.io.exporter.api.ExportController;
+import org.gephi.io.exporter.plugin.ExporterGEXF;
 import org.gephi.project.api.ProjectController;
 import org.gephi.project.api.Workspace;
 import org.openide.util.Exceptions;
@@ -50,7 +53,7 @@ public class VOSViewerJsonToGexf {
     Path filePath;
     InputStream is;
     JsonObject jsonObject;
-    String gexf;
+    String jsonVVAsString;
     Graph graph;
     GraphModel model;
     Set<String> scoresKeys = new HashSet();
@@ -65,66 +68,55 @@ public class VOSViewerJsonToGexf {
     boolean descriptionPresent;
     boolean clusterPresent;
     boolean urlPresent;
+    Workspace workspace;
 
     public VOSViewerJsonToGexf(Path filePath) {
         this.filePath = filePath;
     }
 
-    public VOSViewerJsonToGexf(InputStream is) {
-        this.is = is;
+    public VOSViewerJsonToGexf(String jsonVVAsString) {
+        this.jsonVVAsString = jsonVVAsString;
     }
 
     public String convertToGexf() throws FileNotFoundException {
-        defaultNodeAttributes = Set.of("id", "label", "description", "url", "x", "y", "cluster", "weights", "scores");
-        load();
-        gexfInitiate();
-        getNodeAttributes();
-        turnNodeJsonValuesToGexf();
-        turnEdgeJsonValuesToGexf();
-
-        return gexf;
-    }
-
-    private void load() throws FileNotFoundException {
+        String resultGexf = "";
         try {
-            JsonReader jsonReader = null;
-            UnicodeBOMInputStream ubis = null;
-            if (is == null) {
-                try {
-                    FileInputStream fis = new FileInputStream(filePath.toString());
-                    ubis = new UnicodeBOMInputStream(fis);
-                } catch (NullPointerException | IOException ex) {
-                    Exceptions.printStackTrace(ex);
-                }
-            } else {
-                try {
-                    ubis = new UnicodeBOMInputStream(is);
-                } catch (NullPointerException | IOException ex) {
-                    Exceptions.printStackTrace(ex);
-                }
+            defaultNodeAttributes = Set.of("id", "label", "description", "url", "x", "y", "cluster", "weights", "scores");
+            load();
+            gexfInitiate();
+            getNodeAttributes();
+            turnNodeJsonValuesToGexf();
+            turnEdgeJsonValuesToGexf();
 
-            }
-            InputStreamReader isr = new InputStreamReader(ubis);
-            BufferedReader br = new BufferedReader(isr);
-
-            ubis.skipBOM();
-
-            List<String> lines = br.lines().collect(toList());
-
-            String jsonFixed = String.join("\n", lines).replace("\"\"", "\\\"");
-//            System.out.println("jsonFixed: " + jsonFixed);
-            jsonReader = Json.createReader(new StringReader(jsonFixed));
-            jsonObject = jsonReader.readObject();
+            ExportController ec = Lookup.getDefault().lookup(ExportController.class);
+            ExporterGEXF exporterGexf = (ExporterGEXF) ec.getExporter("gexf");
+            exporterGexf.setWorkspace(workspace);
+            exporterGexf.setExportDynamic(false);
+            exporterGexf.setExportPosition(true);
+            exporterGexf.setExportSize(true);
+            exporterGexf.setExportColors(true);
+            exporterGexf.setExportMeta(true);
+            StringWriter stringWriter = new StringWriter();
+            ec.exportWriter(stringWriter, exporterGexf);
+            stringWriter.close();
+            resultGexf = stringWriter.toString();
         } catch (IOException ex) {
             Exceptions.printStackTrace(ex);
         }
+        return resultGexf;
+    }
+
+    private void load() throws FileNotFoundException {
+        JsonReader jsonReader = Json.createReader(new StringReader(jsonVVAsString));
+        jsonObject = jsonReader.readObject();
     }
 
     private void gexfInitiate() {
         ProjectController pc = Lookup.getDefault().lookup(ProjectController.class);
         pc.newProject();
-        Workspace workspace = pc.getCurrentWorkspace();
-        model = Lookup.getDefault().lookup(GraphController.class).getGraphModel();
+        workspace = pc.getCurrentWorkspace();
+        workspace.getProject().getProjectMetadata().setAuthor("created with nocodefunctions.com");
+        model = Lookup.getDefault().lookup(GraphController.class).getGraphModel(workspace);
         graph = model.getGraph();
 
     }
@@ -142,44 +134,75 @@ public class VOSViewerJsonToGexf {
         Iterator<JsonValue> iteratorItems = items.iterator();
         while (iteratorItems.hasNext()) {
             JsonObject item = (JsonObject) iteratorItems.next();
-            Column stringAttribute;
             for (String key : item.keySet()) {
+
+                // these are treated at the step of node creation / we don't record them as attributes
+                if (key.equals("id") || key.equals("label")) {
+                    continue;
+                }
                 if (!descriptionPresent && key.equals("description")) {
                     descriptionPresent = true;
                     Column attribute = model.getNodeTable().addColumn("description", String.class);
                     attrListNodesAsString.add("description");
                     mapAttributeIdToNodeAttribute.put(key, attribute);
-                }
-
-                if (!urlPresent && key.equals("url")) {
+                } else if (!urlPresent && key.equals("url")) {
                     urlPresent = true;
                     Column attribute = model.getNodeTable().addColumn("url", String.class);
                     attrListNodesAsString.add("url");
                     mapAttributeIdToNodeAttribute.put(key, attribute);
 
-                }
-                if (!clusterPresent && key.equals("cluster")) {
+                } else if (!clusterPresent && key.equals("cluster")) {
                     clusterPresent = true;
                     Column attribute = model.getNodeTable().addColumn("cluster", Integer.class);
                     mapAttributeIdToNodeAttribute.put(key, attribute);
-                }
-                if (!defaultNodeAttributes.contains(key) && !attrListNodesAsString.contains(key)) {
-                    Column attribute = model.getNodeTable().addColumn(key, String.class);
-                    attrListNodesAsString.add(key);
-                    mapAttributeIdToNodeAttribute.put(key, attribute);
-                }
-                if (key.equals("scores")) {
+                } else if (key.equals("scores")) {
                     JsonObject scores = item.getJsonObject("scores");
                     for (String scoreKey : scores.keySet()) {
                         scoresKeys.add(scoreKey);
                     }
-                }
-                if (key.equals("weights")) {
+                } else if (key.equals("weights")) {
                     JsonObject weights = item.getJsonObject("weights");
                     for (String weightKey : weights.keySet()) {
                         weightsKeys.add(weightKey);
                     }
+                } else if (!defaultNodeAttributes.contains(key) && !attrListNodesAsString.contains(key)) {
+                    JsonValue value = item.get(key);
+                    if (value == null) {
+                        continue;
+                    }
+                    if (key.equals("Id")) {
+                        key = "id-attribute-from-gexf";
+                    } else if (key.equals("Label")) {
+                        key = "label-attribute-from-gexf";
+                    }
+
+                    if (attrListNodesAsString.contains(key)) {
+                        continue;
+                    }
+
+                    JsonValue.ValueType valueType = value.getValueType();
+                    Column attribute;
+                    switch (valueType) {
+                        case NUMBER -> {
+                            attribute = model.getNodeTable().addColumn(key, Double.class);
+                        }
+                        case STRING -> {
+                            attribute = model.getNodeTable().addColumn(key, String.class);
+                        }
+                        case TRUE -> {
+                            attribute = model.getNodeTable().addColumn(key, Boolean.class);
+                        }
+                        case FALSE -> {
+                            attribute = model.getNodeTable().addColumn(key, Boolean.class);
+                        }
+                        default -> {
+                            attribute = model.getNodeTable().addColumn(key, Object.class);
+                        }
+                    }
+                    attrListNodesAsString.add(key);
+                    mapAttributeIdToNodeAttribute.put(key, attribute);
                 }
+
             }
         }
 
@@ -222,69 +245,116 @@ public class VOSViewerJsonToGexf {
             }
 
             Node nodeGexf = model.factory().newNode(idString);
-            if (keySet.contains("label")) {
-                nodeGexf.setLabel(item.getString("label"));
-            }
-            if (keySet.contains("description")) {
-                Column att = mapAttributeIdToNodeAttribute.get("description");
-                if (item.isNull("description")) {
-                    nodeGexf.setAttribute(att, "");
-                } else {
-                    nodeGexf.setAttribute(att, item.getString("description"));
-                }
-            }
-
-            if (keySet.contains("url")) {
-                Column att = mapAttributeIdToNodeAttribute.get("url");
-                if (item.isNull("url")) {
-                    nodeGexf.setAttribute(att, "");
-                } else {
-                    nodeGexf.setAttribute(att, item.getString("url"));
-                }
-            }
-
-            if (keySet.contains("x")) {
-                nodeGexf.setX(item.getJsonNumber("x").bigDecimalValue().floatValue());
-            }
-            if (keySet.contains("y")) {
-                nodeGexf.setY(item.getJsonNumber("y").bigDecimalValue().floatValue());
-            }
             if (keySet.contains("x") && keySet.contains("y")) {
                 nodeGexf.setZ(0.0f);
             }
-
-            if (keySet.contains("cluster")) {
-                Column att = mapAttributeIdToNodeAttribute.get("cluster");
-                if (item.isNull("cluster")) {
-                    nodeGexf.setAttribute(att, 0);
-                } else {
-                    nodeGexf.setAttribute(att, item.getInt("cluster"));
+            for (String key : keySet) {
+                if (key.equals("Id")) {
+                    key = "id-attribute-from-gexf";
+                } else if (key.equals("Label")) {
+                    key = "label-attribute-from-gexf";
                 }
-            }
 
-            if (keySet.contains("scores")) {
-                JsonObject scoresObject = item.getJsonObject("scores");
-                for (String key : scoresObject.keySet()) {
-                    Column att = mapAttributeIdToScoreAttribute.get(key);
-                    if (scoresObject.isNull(key)) {
-                        nodeGexf.setAttribute(att, 0d);
-                    } else {
-                        nodeGexf.setAttribute(att, scoresObject.getJsonNumber(key).doubleValue());
+                switch (key) {
+                    case "label" ->
+                        nodeGexf.setLabel(item.getString("label"));
+                    case "description" -> {
+                        Column att = mapAttributeIdToNodeAttribute.get("description");
+                        if (item.isNull("description")) {
+                            nodeGexf.setAttribute(att, "");
+                        } else {
+                            nodeGexf.setAttribute(att, item.getString("description"));
+                        }
+                    }
+                    case "url" -> {
+                        Column att = mapAttributeIdToNodeAttribute.get("url");
+                        if (item.isNull("url")) {
+                            nodeGexf.setAttribute(att, "");
+                        } else {
+                            nodeGexf.setAttribute(att, item.getString("url"));
+                        }
+                    }
+                    case "cluster" -> {
+                        Column att = mapAttributeIdToNodeAttribute.get("cluster");
+                        if (item.isNull("cluster")) {
+                            nodeGexf.setAttribute(att, 0);
+                        } else {
+                            nodeGexf.setAttribute(att, item.getInt("cluster"));
+                        }
+                    }
+                    case "scores" -> {
+                        JsonObject scoresObject = item.getJsonObject("scores");
+                        for (String keyScore : scoresObject.keySet()) {
+                            Column att = mapAttributeIdToScoreAttribute.get(keyScore);
+                            if (scoresObject.isNull(keyScore)) {
+                                nodeGexf.setAttribute(att, 0d);
+                            } else {
+                                nodeGexf.setAttribute(att, scoresObject.getJsonNumber(keyScore).doubleValue());
+                            }
+                        }
+                    }
+                    case "weights" -> {
+                        JsonObject weightsObject = item.getJsonObject("weights");
+                        for (String keyWeight : weightsObject.keySet()) {
+                            Column att = mapAttributeIdToWeightAttribute.get(keyWeight);
+                            if (weightsObject.isNull(keyWeight)) {
+                                nodeGexf.setAttribute(att, 0d);
+                            } else {
+                                nodeGexf.setAttribute(att, weightsObject.getJsonNumber(keyWeight).doubleValue());
+                            }
+                        }
+                    }
+                    case "x" ->
+                        nodeGexf.setX(item.getJsonNumber("x").bigDecimalValue().floatValue());
+                    case "y" ->
+                        nodeGexf.setY(item.getJsonNumber("y").bigDecimalValue().floatValue());
+                    case "id" -> {
+                    }
+                    case "id-attribute-from-gexf" -> {
+                        Column att = mapAttributeIdToNodeAttribute.get(key);
+                        if (att.getTypeClass() == String.class) {
+                            nodeGexf.setAttribute(att, item.getString("Id"));
+                        } else if (att.getTypeClass() == Double.class || att.getTypeClass() == Float.class) {
+                            nodeGexf.setAttribute(att, item.getJsonNumber("Id").doubleValue());
+                        } else if (att.getTypeClass() == Integer.class) {
+                            nodeGexf.setAttribute(att, item.getJsonNumber("Id").intValue());
+                        } else if (att.getTypeClass() == Long.class) {
+                            nodeGexf.setAttribute(att, item.getJsonNumber("Id").longValue());
+                        } else if (att.getTypeClass() == Boolean.class) {
+                            nodeGexf.setAttribute(att, item.getBoolean("Id"));
+                        }
+                    }
+                    case "label-attribute-from-gexf" -> {
+                        Column att = mapAttributeIdToNodeAttribute.get(key);
+                        if (att.getTypeClass() == String.class) {
+                            nodeGexf.setAttribute(att, item.getString("Label"));
+                        } else if (att.getTypeClass() == Double.class || att.getTypeClass() == Float.class) {
+                            nodeGexf.setAttribute(att, item.getJsonNumber("Label").doubleValue());
+                        } else if (att.getTypeClass() == Integer.class) {
+                            nodeGexf.setAttribute(att, item.getJsonNumber("Label").intValue());
+                        } else if (att.getTypeClass() == Long.class) {
+                            nodeGexf.setAttribute(att, item.getJsonNumber("Label").longValue());
+                        } else if (att.getTypeClass() == Boolean.class) {
+                            nodeGexf.setAttribute(att, item.getBoolean("Label"));
+                        }
+                    }
+                    default -> {
+                        Column att = mapAttributeIdToNodeAttribute.get(key);
+                        if (att.getTypeClass() == String.class) {
+                            nodeGexf.setAttribute(att, item.getString(key));
+                        } else if (att.getTypeClass() == Double.class || att.getTypeClass() == Float.class) {
+                            nodeGexf.setAttribute(att, item.getJsonNumber(key).doubleValue());
+                        } else if (att.getTypeClass() == Integer.class) {
+                            nodeGexf.setAttribute(att, item.getJsonNumber(key).intValue());
+                        } else if (att.getTypeClass() == Long.class) {
+                            nodeGexf.setAttribute(att, item.getJsonNumber(key).longValue());
+                        } else if (att.getTypeClass() == Boolean.class) {
+                            nodeGexf.setAttribute(att, item.getBoolean(key));
+                        }
                     }
                 }
             }
-
-            if (keySet.contains("weights")) {
-                JsonObject weightsObject = item.getJsonObject("weights");
-                for (String key : weightsObject.keySet()) {
-                    Column att = mapAttributeIdToWeightAttribute.get(key);
-                    if (weightsObject.isNull(key)) {
-                        nodeGexf.setAttribute(att, 0d);
-                    } else {
-                        nodeGexf.setAttribute(att, weightsObject.getJsonNumber(key).doubleValue());
-                    }
-                }
-            }
+            graph.addNode(nodeGexf);
         }
     }
 
@@ -329,6 +399,8 @@ public class VOSViewerJsonToGexf {
             Node node2 = graph.getNode(targetIdString);
             Edge edge = model.factory().newEdge(node1, node2, false);
             edge.setWeight(link.getJsonNumber("strength").bigDecimalValue().floatValue());
+            graph.addEdge(edge);
+
         }
     }
 }
